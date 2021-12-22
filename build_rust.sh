bdec6fb059cbcc314d265ef2d46001f32a802cd5
Matt Bray <mattjbray@gmail.com>
2021-12-22 22:22:11 +0000

    fix: use printf

--- build_rust.sh
+++ build_rust.sh
@@ -2,8 +2,7 @@
 
 if [ "`uname`" = "Darwin" ]; then
   CAML_LIB="`ocamlc -where`";
-  SEP="`echo '\x1f'`";
-  CARGO_ENCODED_RUSTFLAGS="-L$CAML_LIB$SEP-lcamlrun" cargo build --release --frozen ;
+  CARGO_ENCODED_RUSTFLAGS="$(printf -- "-L%s\x1f-lcamlrun" "$CAML_LIB")" cargo build --release --frozen ;
 else
   cargo build --release --frozen ;
 fi
